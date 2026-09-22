a9c398e067883595fbf4177182a39b794c774ae9
Encore Projects <amine213sj@gmail.com>
2026-09-22 22:51:24 +0200

    Change ENCORE_CA to ENCORE_CA_URL with API endpoint
    Updated ENCORE_CA variable to ENCORE_CA_URL with API link.

--- ca.js
+++ ca.js
@@ -4,7 +4,7 @@
 // automatiquement en ~1 minute. Tant que c'est vide, le site montre "Drops at launch".
 //
 // Exemple :  window.ENCORE_CA = "3nv6CtYWRacohRqY3mPdxDc13DxFSy18GYWTkQKuqd5c";
-window.ENCORE_CA = "";
+window.ENCORE_CA_URL = "https://api.npoint.io/xxxxxxxx";
 
 // (Optionnel) LIEN "Trade" : colle ici le lien de la page où on peut acheter/trader
 // ton token (ex. ta page sur l'app Robinhood Chain, ou le DEX que tu utilises).
